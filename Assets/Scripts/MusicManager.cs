9cae6d4570b3d16304ab8d7ffdc3ccea5943c577
GAmuzak <thatamuzak@gmail.com>
2021-02-20 23:05:12 +0530

    moosik

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MusicClip {
+    Null=-1, bracc_init, bracc_final
+}
+public class MusicManager : Singleton<MusicManager>
+{
+    private AudioSource musicSource;
+
+    [SerializeField]
+    private List<AudioClip> clips;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(this);
+        musicSource = GetComponent<AudioSource>();
+    }
+    public void SetClipToPlay(MusicClip m)
+    {
+        musicSource.clip = clips[(int)m];
+        musicSource.Play();
+    }
+}
---
+++ Assets/Scripts/SetMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMusic : MonoBehaviour
+{
+    [SerializeField]
+    private MusicClip clipToPlay=0;
+    void Start()
+    {
+        SetClipToPlay();
+    }
+    [ContextMenu(nameof(SetClipToPlay))]
+    private void SetClipToPlay()
+    {
+        MusicManager.Instance.SetClipToPlay(clipToPlay);
+    }
+}
